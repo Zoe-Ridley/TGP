235db0c63be7ebc36faabcbdbf9365c4247a0f61
Issac <gundam.issacwai@gmail.com>
2022-01-22 22:08:20 +0000

    Added Audio Manager

---
+++ TGP/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Audio[] AudioClip;
+
+    private void Awake()
+    {
+        foreach (Audio a in AudioClip)
+        {
+            a.source = gameObject.AddComponent<AudioSource>();
+            a.source.clip = a.Sound;
+            a.source.volume = a.volume;
+        }
+    }
+
+    public void playAudio(string name)
+    {
+        foreach (Audio a in AudioClip)
+        {
+            if (a.m_Name == name)
+            {
+                a.source.Play();
+            }
+        }
+    }
+}
