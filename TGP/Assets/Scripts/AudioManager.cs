a677f4ad39f5aedd9b81a4e9fa3760e9ee82ff45
Issac <gundam.issacwai@gmail.com>
2022-01-25 19:06:34 +0000

    Completed Volume and SFX slider

--- TGP/Assets/Scripts/AudioManager.cs
+++ TGP/Assets/Scripts/AudioManager.cs
@@ -7,14 +7,41 @@ public class AudioManager : MonoBehaviour
 {
     public Audio[] AudioClip;
 
+    public static AudioManager instance;
+
+    [SerializeField] AudioMixerGroup m_MusicMixerGroup;
+    [SerializeField] AudioMixerGroup m_SFXMixerGroup;
+
     private void Awake()
     {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         foreach (Audio a in AudioClip)
         {
             a.source = gameObject.AddComponent<AudioSource>();
             a.source.clip = a.Sound;
             a.source.volume = a.volume;
             a.source.loop = a.loop;
+
+            switch (a.audioType)
+            {
+                case Audio.AudioTypes.Music:
+                    a.source.outputAudioMixerGroup = m_MusicMixerGroup;
+                    break;
+                case Audio.AudioTypes.SFX:
+                    a.source.outputAudioMixerGroup = m_SFXMixerGroup;
+                    break;
+            }
         }
     }
 
@@ -32,6 +59,12 @@ public class AudioManager : MonoBehaviour
                 a.source.Play();
             }
         }
+    }
+
+    public void UpdateMixerVolume()
+    {
+        m_MusicMixerGroup.audioMixer.SetFloat("Music Volume", Mathf.Log10(VolumeSlider.musicVolume) * 20);
+        m_SFXMixerGroup.audioMixer.SetFloat("SFX Volume", Mathf.Log10(VolumeSlider.SFXVolume) * 20);
     }
 }
 
