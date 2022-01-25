7bb2ee5c8f1d15efa0e8801850b88251372c578f
Zoe-Ridley <zoeridley@live.com>
2022-01-25 19:04:14 +0000

    opening doors on click
    slightly broken. algorithm now breaks when the dungeon generated has a y of 1

--- TGP/Assets/Scripts/DungeonGenerator.cs
+++ TGP/Assets/Scripts/DungeonGenerator.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+[Serializable]
 public class DungeonGenerator : MonoBehaviour
 {
     public class Cell
@@ -13,12 +15,6 @@ public class DungeonGenerator : MonoBehaviour
         public bool[] m_generatedRoomStatus = new bool[4];
         public bool[] m_closedRoomStatus = {false, false, false, false};
         public Vector2 m_Position;
-    }
-
-    public struct Room
-    {
-        public Cell m_cell;
-        public int m_boardPosition;
     };
 
     public Vector2 m_size;
@@ -34,35 +30,60 @@ public class DungeonGenerator : MonoBehaviour
         MazeGenerator();
     }
 
-    public void OpenRoom(GameObject searchRoom)
+    public void OpenRoom(Vector2 tempPlayerPosition)
     {
-        Vector2 desiredLocation = new Vector2(Mathf.FloorToInt(searchRoom.transform.position.x), Mathf.FloorToInt(searchRoom.transform.position.y));
+        //search room is the room prefab instance that the player is currently stood inside
+        //find its int position and compare it to all tiles in the board
+        //Vector2 desiredLocation = new Vector2(Mathf.FloorToInt(searchRoom.transform.position.x), Mathf.FloorToInt(searchRoom.transform.position.y));
+
+        tempPlayerPosition.x = Mathf.FloorToInt(tempPlayerPosition.x / m_offset.x);
+        tempPlayerPosition.y = Mathf.FloorToInt(tempPlayerPosition.y / m_offset.y);
+
+        if (tempPlayerPosition.y < 0)
+        {
+            tempPlayerPosition.y *= -1;
+        }
+
         for (int i = 0; i < m_board.Count; i++)
         {
-            if (desiredLocation == m_board[i].m_Position)
+            if (tempPlayerPosition == m_board[i].m_Position)
             {
-                searchRoom.GetComponent<RoomBehaviour>().UpdateRoom(m_board[i].m_generatedRoomStatus);
+                //open the door for the room currently stood inside
+                GameObject playerStoodRoom = GameObject.Find("Room(Clone) " + (tempPlayerPosition.x) + "-" + (tempPlayerPosition.y));
+                playerStoodRoom.GetComponent<RoomBehaviour>().UpdateRoom(m_board[i].m_generatedRoomStatus);
+
+                /*finding the next cell to open the corresponding door. without this only the one door
+                 inside the current room is opened, and the player cannot go into the next room*/
                 for (int j = 0; j < 4; j++)
                 {
                     if (m_board[i].m_Position.x + 1 == m_board[i + 1].m_Position.x)
                     {
                         //next cell in the path is to the right
+                        GameObject nextRoom = GameObject.Find("Room(Clone) " + (m_board[i+1].m_Position.x) + "-" + (m_board[i+1].m_Position.y));
+                        nextRoom.GetComponent<RoomBehaviour>().UpdateRoom(new[] { false, false, false, true });
                     }
                     else if (m_board[i].m_Position.x - 1 == m_board[i + 1].m_Position.x)
                     {
                         //next cell in the path is to the left
+                        GameObject nextRoom = GameObject.Find("Room(Clone) " + (m_board[i+1].m_Position.x) + "-" + (m_board[i+1].m_Position.y ));
+                        nextRoom.GetComponent<RoomBehaviour>().UpdateRoom(new[] { false, false, true, false });
                     }
                     else if (m_board[i].m_Position.y + 1 == m_board[i + 1].m_Position.y)
                     {
                         //next cell in the path is above
+                        GameObject nextRoom = GameObject.Find("Room(Clone) " + (m_board[i+1].m_Position.x) + "-" + (m_board[i+1].m_Position.y));
+                        nextRoom.GetComponent<RoomBehaviour>().UpdateRoom(new[] { false, true, false, false });
                     }
                     else if (m_board[i].m_Position.y - 1 == m_board[i - 1].m_Position.y)
                     {
                         //next call is below
-                    } 
-
+                        GameObject nextRoom = GameObject.Find("Room(Clone) " + (m_board[i+1].m_Position.x) + "-" + (m_board[i+1].m_Position.y));
+                        nextRoom.GetComponent<RoomBehaviour>().UpdateRoom(new[] { true, false, false, false });
+                    }
                 }
                 break;
+
+                //0 Up, 1 Down, 2 Right, 3 Left
             }
         }
     }
@@ -77,9 +98,8 @@ public class DungeonGenerator : MonoBehaviour
                 if (currentCell.m_visited)
                 {
                     var newRoom = Instantiate(m_room, new Vector3(i * m_offset.x, -j * m_offset.y, 0f), Quaternion.identity, transform).GetComponent<RoomBehaviour>();
-                    //newRoom.UpdateRoom(currentCell.m_generatedRoomStatus);
-                    newRoom.UpdateRoom(currentCell.m_closedRoomStatus);
-
+                    newRoom.UpdateRoom(currentCell.m_generatedRoomStatus);
+                    //newRoom.UpdateRoom(currentCell.m_closedRoomStatus);
                     newRoom.name += " " + i + "-" + j;
                 }
             }
--- TGP/Assets/Scripts/RoomUnlock.cs
+++ TGP/Assets/Scripts/RoomUnlock.cs
@@ -14,16 +14,7 @@ public class RoomUnlock : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(new Vector2(m_player.transform.position.x - 1f, m_player.transform.position.y - 1f),
-                            Vector2.left, 1000.0f);
-            Debug.Log(hit.point);
-            Debug.Log("click! :D");
-            if (hit.collider != null)
-            {
-                GameObject tempObject = hit.transform.parent.transform.parent.gameObject;
-                Debug.Log(tempObject.name);
-                m_dungeonGenerator.OpenRoom(tempObject);
-            }
+            m_dungeonGenerator.OpenRoom(new Vector2(m_player.transform.position.x, m_player.transform.position.y));
         }
     }
 }
