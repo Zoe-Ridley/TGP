a5769b9637dac1a694982a4ae20c20a69e82ebae
Tuffy137 <Tuffy137@users.noreply.github.com>
2022-01-26 15:49:30 +0000

    More cleanup on the main scene, fixing some stuff that went wacky when merging

--- TGP/Assets/Scripts/Shoot.cs
+++ TGP/Assets/Scripts/Shoot.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     [Header("Bullet variables")]
-    [SerializeField] private GameObject m_BulletPrefab;
-    [SerializeField] private float m_FiringForce;
+    [SerializeField] private GameObject m_bulletPrefab;
+    [SerializeField] private float m_firingForce;
     [SerializeField] private float m_coolDownTime;
     private float m_coolDown;
 
@@ -20,8 +20,8 @@ public class Shoot : MonoBehaviour
             //All this works and fires a bullet prefab towards the mouse location
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
             Vector2 toMouse = mousePos - (Vector2)transform.position;
-            GameObject tempRef = Instantiate<GameObject>(m_BulletPrefab, gameObject.transform.position, Quaternion.LookRotation(Vector3.forward, Vector3.Cross(Vector3.forward, toMouse.normalized)));
-            tempRef.GetComponent<Rigidbody2D>().AddForce(tempRef.transform.right * m_FiringForce, ForceMode2D.Impulse);
+            GameObject tempRef = Instantiate<GameObject>(m_bulletPrefab, gameObject.transform.position, Quaternion.LookRotation(Vector3.forward, Vector3.Cross(Vector3.forward, toMouse.normalized)));
+            tempRef.GetComponent<Rigidbody2D>().AddForce(tempRef.transform.right * m_firingForce, ForceMode2D.Impulse);
             m_coolDown = m_coolDownTime;
         }
     }
